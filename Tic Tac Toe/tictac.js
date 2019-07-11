1f5dfe7cfe3a1ddd6c9587c80dfc410fded09d2d
Xavalia <51314803+Xavalia@users.noreply.github.com>
2019-07-11 11:08:11 +0200

    Add files via upload

---
+++ Tic Tac Toe/tictac.js
@@ -0,0 +1,9 @@
+var uwin = prompt ("fais un tic tac toe java s'il te plaît"),
+var ligne = ["L","L","L","L","L","L","L","L"]
+
+     if (ligne= str.lengt >= 3) 
+     	alert ("win");
+
+     else 
+     	alert ("try again");
+
